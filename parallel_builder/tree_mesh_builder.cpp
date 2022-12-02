e591880cec10379b642c0c40fde2789e966a457b
Alexandra <alexandra.slezak@gmail.com>
2022-12-02 22:36:39 +0100

    tree

--- parallel_builder/tree_mesh_builder.cpp
+++ parallel_builder/tree_mesh_builder.cpp
@@ -15,27 +15,88 @@
 #include "tree_mesh_builder.h"
 
 TreeMeshBuilder::TreeMeshBuilder(unsigned gridEdgeSize)
-    : BaseMeshBuilder(gridEdgeSize, "Octree")
+        : BaseMeshBuilder(gridEdgeSize, "Octree")
 {
 
+}
+
+unsigned TreeMeshBuilder::decomposeTree(const Vec3_t<float> &pos,
+                                        const ParametricScalarField &field,
+                                        const unsigned &gridSize)
+{
+    unsigned totalTriangles = 0;
+
+    unsigned gridSize_ = gridSize / 2;
+    auto gridSizeF = float(gridSize_);
+
+    float x = (pos.x + gridSizeF) * mGridResolution;
+    float y = (pos.y + gridSizeF) * mGridResolution;
+    float z = (pos.z + gridSizeF) * mGridResolution;
+
+    Vec3_t<float> midPoint(x, y, z);
+    float f = field.getIsoLevel() + ((sqrt(3) / 2) * (gridSize * mGridResolution));
+
+    /* check if the block is empty */
+    if (evaluateFieldAt(midPoint, field) > f) {
+        return 0;
+    }
+
+    /* cut off */
+    if (gridSize <= 1) {
+        return buildCube(pos, field);
+    }
+
+    for (Vec3_t<float> point : sc_vertexNormPos) {
+        #pragma omp task default(none) \
+            shared(pos, field, gridSize_, gridSizeF, totalTriangles) \
+            firstprivate(point)
+        {
+            Vec3_t<float> cubeOffset(pos.x + point.x * gridSizeF,
+                                     pos.y + point.y * gridSizeF,
+                                     pos.z + point.z * gridSizeF);
+
+            unsigned triangles = decomposeTree(cubeOffset, field, gridSize_);
+            #pragma omp critical
+            totalTriangles += triangles;
+        }
+    }
+
+    #pragma omp taskwait
+    return totalTriangles;
 }
 
 unsigned TreeMeshBuilder::marchCubes(const ParametricScalarField &field)
 {
-    // Suggested approach to tackle this problem is to add new method to
-    // this class. This method will call itself to process the children.
-    // It is also strongly suggested to first implement Octree as sequential
-    // code and only when that works add OpenMP tasks to achieve parallelism.
-    
-    return 0;
+    unsigned trianglesCount;
+
+    #pragma omp parallel default(none) shared(trianglesCount, field)
+    #pragma omp single nowait
+    trianglesCount = decomposeTree(Vec3_t<float>(), field, mGridSize);
+
+    return trianglesCount;
 }
 
 float TreeMeshBuilder::evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field)
 {
-    return 0.0f;
+    const Vec3_t<float> *pPoints = field.getPoints().data();
+    const unsigned count = unsigned(field.getPoints().size());
+
+    float value = std::numeric_limits<float>::max();
+
+    for(unsigned i = 0; i < count; ++i)
+    {
+        float distanceSquared  = (pos.x - pPoints[i].x) * (pos.x - pPoints[i].x);
+        distanceSquared       += (pos.y - pPoints[i].y) * (pos.y - pPoints[i].y);
+        distanceSquared       += (pos.z - pPoints[i].z) * (pos.z - pPoints[i].z);
+
+        value = std::min(value, distanceSquared);
+    }
+
+    return sqrt(value);
 }
 
 void TreeMeshBuilder::emitTriangle(const BaseMeshBuilder::Triangle_t &triangle)
 {
-    
+    #pragma omp critical
+    mTriangles.push_back(triangle);
 }
