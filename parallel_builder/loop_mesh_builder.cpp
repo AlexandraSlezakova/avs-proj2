e7ce3da8a63a128d864c281e9d47c54fa045f79d
Alexandra <alexandra.slezak@gmail.com>
2022-12-02 22:36:34 +0100

    loop

--- parallel_builder/loop_mesh_builder.cpp
+++ parallel_builder/loop_mesh_builder.cpp
@@ -22,15 +22,48 @@ LoopMeshBuilder::LoopMeshBuilder(unsigned gridEdgeSize)
 
 unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
 {
-    return 0;
+    size_t totalCubesCount = mGridSize * mGridSize * mGridSize;
+
+    unsigned totalTriangles = 0;
+
+    #pragma omp parallel for default(none) \
+        shared(field, totalCubesCount) \
+        schedule(static) reduction(+: totalTriangles)
+    for(size_t i = 0; i < totalCubesCount; ++i)
+    {
+        Vec3_t<float> cubeOffset( i % mGridSize,
+                                  (i / mGridSize) % mGridSize,
+                                  i / (mGridSize*mGridSize));
+
+        unsigned trianglesCount = buildCube(cubeOffset, field);
+        #pragma omp critical
+        totalTriangles += trianglesCount;
+    }
+
+    return totalTriangles;
 }
 
 float LoopMeshBuilder::evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field)
 {
-    return 0.0f;
+    const Vec3_t<float> *pPoints = field.getPoints().data();
+    const unsigned count = unsigned(field.getPoints().size());
+
+    float value = std::numeric_limits<float>::max();
+
+    for(unsigned i = 0; i < count; ++i)
+    {
+        float distanceSquared  = (pos.x - pPoints[i].x) * (pos.x - pPoints[i].x);
+        distanceSquared       += (pos.y - pPoints[i].y) * (pos.y - pPoints[i].y);
+        distanceSquared       += (pos.z - pPoints[i].z) * (pos.z - pPoints[i].z);
+
+        value = std::min(value, distanceSquared);
+    }
+
+    return sqrt(value);
 }
 
 void LoopMeshBuilder::emitTriangle(const BaseMeshBuilder::Triangle_t &triangle)
 {
-
+    #pragma omp critical
+    mTriangles.push_back(triangle);
 }
